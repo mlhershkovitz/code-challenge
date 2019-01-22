7e65af807ff224bb984e936de05b24eae2ff8fae
Madison Hershkovitz <mlhersh6@gmail.com>
2019-01-22 15:29:06 -0600

    finished step 1

---
+++ server/public/scripts/client.js
@@ -0,0 +1,14 @@
+$(document).ready(readyNow);
+
+function readyNow() {
+    //need button on click
+    $(`#submit-btn`).on('click', submitGet);
+    //need to update p tag text when page loads
+    $(`.welcome`).append(`<p>The page has loaded</p>`);
+}; //end readyNow
+
+//button function
+//need ajax GET request
+function submitGet() {
+    console.log('button clicked');
+}; //end submitGet
